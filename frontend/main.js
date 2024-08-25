9e3e9f7c3cae82a65899c51526b205d6c14a4ca1
jedi <jedi2454@gmail.com>
2024-08-25 14:10:30 -0400

    Add github Action

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,9 @@
-const functionApi = ''; // Add your API endpoint here
-
+// const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter'; // Add your API endpoint here
+const functionApiUrl = 'https://cloudresumefunction.azurewebsites.net/api/GetResumeCounter?code=7OkEawjvU66ET_kLwlAw3stb9lTr3EvpiWjw7buTLodZAzFucbKwPQ%3D%3D';
+                                                
 const getVisitorCount = async () => {
     try {
-        const response = await fetch(functionApi);
+        const response = await fetch(functionApiUrl);
         const data = await response.json();
         console.log(data);
         let count = data.count;
